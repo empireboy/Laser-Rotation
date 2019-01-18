b649d50c1dc92e1502f009ab6a92ed6af20978b3
DESKTOP-5SM091D\cemel <cemeloen@gmail.com>
2019-01-18 11:52:14 +0100

    Made the RhythmController reusable

--- Assets/Scripts/RhythmController.cs
+++ Assets/Scripts/RhythmController.cs
@@ -1,13 +1,10 @@
 ﻿using UnityEngine;
-using System.Collections.Generic;
 
 public class RhythmController : MonoBehaviour
 {
 	[SerializeField]
 	private Level _level;
 	public Level Level { get => _level; }
-	//public float secondsToReachTarget = 1f;
-	//public float slownessFactor = 1;
 
 	public delegate void BeatHandler(int beat);
 	public event BeatHandler BeatEvent;
@@ -17,17 +14,6 @@ public class RhythmController : MonoBehaviour
 
 	private float _secondsPerBeat;
 	public float SecondsPerBeat { get => _secondsPerBeat; }
-
-	//private int _circleCounter = 0;
-
-	[SerializeField]
-	private List<Transform> _circles;
-
-	[SerializeField]
-	private List<int> _laserArray;
-
-	[SerializeField]
-	private List<CircleStruct> _circleArray;
 
 	private void Awake()
 	{
@@ -42,27 +28,8 @@ public class RhythmController : MonoBehaviour
 
 	private void NextBeat()
 	{
-		/*if (_laserArray[_currentBeat] != -1 && _laserArray[_currentBeat] != 999)
-		{
-			GetComponent<SpawnAroundObject>().Spawn(_laserArray[_currentBeat], 5);
-		}
-
-		if (_laserArray[_currentBeat] == 999)
-		{
-			for (int i = 0; i < 4; i++)
-			{
-				_circles[i].gameObject.SetActive(_circleArray[_circleCounter].circleActive[i]);
-			}
-		}*/
-
 		BeatEvent?.Invoke(_currentBeat);
 
 		_currentBeat++;
-	}
-
-	[System.Serializable]
-	public struct CircleStruct
-	{
-		public bool[] circleActive;
 	}
 }
